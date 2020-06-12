ce4be0763cbbb414bf3ea5557e605d1d25a31791
Paul Solt <PaulSolt@gmail.com>
2020-06-12 12:46:17 -0400

    Starting project design

---
+++ iOS16-TL-MapKit-StackViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS16-TL-MapKit-StackViews
+//
+//  Created by Paul Solt on 6/12/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
